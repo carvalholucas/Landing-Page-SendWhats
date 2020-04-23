9d0eed5f442cb0d19621e927bf76ba7aa82d9e81
Lucas Carvalho <lucas.carvalho@azymus.com.br>
2020-04-22 22:16:47 -0300

    Get pages from Rest API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,12 @@
-exports.createPages = async ({ actions: { createPage } }) => {
-    const allUsers = [
-        { name: 'Lucas Carvalho', link: 'lucassmc', numberWhatsapp: '+5519991755721', message: 'Olá, gostária de falar com você' },
-        { name: 'Anna Luiza', link: 'annagomes', numberWhatsapp: '+5519991755721', message: 'Olá, gostária de falar com você' },
-        { name: 'Frontend Criativo', link: 'frontendcriativo', numberWhatsapp: '+5519991755721', message: 'Olá, gostária de falar com você'}
-    ]
+const axios = require('axios')
 
-    allUsers.forEach(user => {
+const getPages = uri => axios.get(uri).then(response => response.data)
+
+exports.createPages = async ({ actions: { createPage } }) => {
+
+    const [pages] = await Promise.all([ getPages('https://sendwhats.herokuapp.com/contacts')])
+
+    pages.forEach(user => {
         createPage({
             path: `/${user.link}/`,
             component: require.resolve("./src/templates/contact-page.js"),
--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const ContactPage = ({ pageContext: { user } }) => (
-    <div style={{ width: 960, margin: "4rem auto" }}>
-        <h1>{user.name}</h1>
-        <p>Envie uma mensagem para {user.name}</p>
-        <br/>
-        <br/>
-        <a href={`https://api.whatsapp.com/send?phone=${user.numberWhatsapp}&text=${user.message}`} target="_blank" rel="noopener noreferrer" style={{ padding: 15, background: '#278a65', color: "#fff", textDecoration: "none"}}>Enviar mensagem</a>
-    </div>
-)
+const ContactPage = ({ pageContext: { user } }) => {
+    return (
+        <div style={{ width: 960, margin: "4rem auto" }}>
+            <h1>Nome</h1>
+            <p>Adicionar o nome no DB e na API</p>
+            <br />
+            <p>{user.link}</p>
+            <br />
+            <br />
+            <a href={`https://api.whatsapp.com/send?phone=${user.number}&text=${user.message}`} target="_blank" rel="noopener noreferrer" style={{ padding: 15, background: '#278a65', color: "#fff", textDecoration: "none" }}>Enviar mensagem</a>
+        </div>
+    )
+}
 
 export default ContactPage
